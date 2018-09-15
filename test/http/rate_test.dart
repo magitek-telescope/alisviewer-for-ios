02c56e434ead18085449f2e0d772d46093c22e70
HANATANI Takuma <mail@potato4d.me>
2018-09-15 15:49:19 +0900

    Add rate client test

---
+++ test/http/rate_test.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart' as http;
+import '../../lib/http/rate.dart';
+
+void main() {
+  test('fetchRate', () async {
+    // FIXME: Implement DI system
+    final RateClient rateClient = new RateClient();
+    await rateClient.fetchRate();
+    Rate rate = rateClient.rate;
+    expect(rate.alisToJpy == 0.0, false);
+    expect(rate.alisToBtc== 0.0, false);
+    expect(rate.btcToJpy == 0.0, false);
+  });
+}
